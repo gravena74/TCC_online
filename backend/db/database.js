995c6eb45c56d5da4df88d1eb25b6ac323de8df5
João Vitor Marco Antonio Gravena <gravena74jv@gmail.com>
2026-09-07 10:24:44 -0300

    Modificação para subir no servidor

--- backend/db/database.js
+++ backend/db/database.js
@@ -2,13 +2,10 @@ import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
- // ssl: { rejectUnauthorized: false },
-  waitForConnections: true,
-  connectionLimit: 10,
+  port: process.env.DB_PORT
 });
 
 // Camada de compatibilidade com a API que o resto do projeto ja usava
